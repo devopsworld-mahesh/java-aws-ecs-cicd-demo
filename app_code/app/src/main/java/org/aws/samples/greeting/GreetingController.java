c6c7423262012cf0cd5e562a3c7a41b1bc6c71a2
Mahesh Raghunandanan <mahrag@amazon.com>
2022-01-28 14:05:42 +0530

    Reverted Java code

--- app_code/app/src/main/java/org/aws/samples/greeting/GreetingController.java
+++ app_code/app/src/main/java/org/aws/samples/greeting/GreetingController.java
@@ -3,8 +3,6 @@ package org.aws.samples.greeting;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 /**
  * @author MaheshR
@@ -14,20 +12,6 @@ public class GreetingController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String sayHello() {
-        return "Hello from AWS ProServe version3";
+        return "Hello from AWS ProServe version2";
     }
-    public static void main(String[] args) {
-
-        InetAddress ip;
-        String hostname;
-        try {
-            ip = InetAddress.getLocalHost();
-            hostname = ip.getHostName();
-            System.out.println("Your current IP address : " + ip);
-            System.out.println("Your current Hostname : " + hostname);
-
-        } catch (UnknownHostException e) {
-
-            e.printStackTrace();
-        }
 }
